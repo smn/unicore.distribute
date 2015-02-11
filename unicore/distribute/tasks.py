87d81d96e4de7736acee4652955bfc5032993d60
Milton Madanda <madandat@gmail.com>
2015-02-11 14:47:10 +0200

    add es_host as a parameter to the fastforward task

--- unicore/distribute/tasks.py
+++ unicore/distribute/tasks.py
@@ -5,8 +5,9 @@ from unicore.content.models import Page, Category, Localisation
 
 
 @app.task(ignore_result=True)
-def fastforward(repo_path, index_prefix):
-    workspace = EG.workspace(repo_path, index_prefix=index_prefix)
+def fastforward(repo_path, index_prefix, es_host='http://localhost:9200'):
+    workspace = EG.workspace(
+        repo_path, index_prefix=index_prefix, es={'urls': [es_host]})
     workspace.fast_forward()
     workspace.reindex(Page)
     workspace.reindex(Category)
