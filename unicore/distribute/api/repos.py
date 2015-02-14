d74cf891d952b1eadf7ccd89f751bd4eb70b46f8
Simon de Haan <simon@praekeltfoundation.org>
2015-02-14 08:31:20 +0200

    make service work

--- unicore/distribute/api/repos.py
+++ unicore/distribute/api/repos.py
@@ -1,29 +1,25 @@
 import os
 
-from cornice import Service
+from cornice.resource import resource, view
+
 
 from unicore.distribute.utils import (
     get_config, get_repositories, get_repository, format_repo)
 
 
-repositories_index = Service(name='repositories_index', path='/repos/',
-                             description='List of repositories')
+@resource(collection_path='/repos/', path='/repos/{name}/')
+class Repository(object):
 
+    def __init__(self, request):
+        self.request = request
+        self.config = get_config(request)
 
-@repositories_index.get()
-def repositories_index(request):
-    config = get_config(request)
-    repo_path = config.get('unicore.distribute', 'repo.storage_path')
-    return [format_repo(repo) for repo in get_repositories(repo_path)]
+    def collection_get(self):
+        repo_path = self.config.get('unicore.distribute', 'repo.storage_path')
+        return [format_repo(repo) for repo in get_repositories(repo_path)]
 
-
-repository = Service(name='repository', path='/repos/{name}/',
-                     description='Repository')
-
-
-@repository.get()
-def repository(request):
-    config = get_config(request)
-    name = request.matchdict['name']
-    repo_path = config.get('unicore.distribute', 'repo.storage_path')
-    return format_repo(get_repository(os.path.join(repo_path, name)))
+    @view(renderer='json')
+    def get(self):
+        name = self.request.matchdict['name']
+        repo_path = self.config.get('unicore.distribute', 'repo.storage_path')
+        return format_repo(get_repository(os.path.join(repo_path, name)))
