1f5d5ed725a6ddc313cf773166d7e41005c4ba7b
Simon de Haan <simon@praekeltfoundation.org>
2015-02-17 22:00:04 +0200

    some tests

--- unicore/distribute/tests/test_utils.py
+++ unicore/distribute/tests/test_utils.py
@@ -3,7 +3,12 @@ import textwrap
 from StringIO import StringIO
 from unittest import TestCase
 
-from unicore.distribute.utils import UCConfigParser
+from unicore.distribute.utils import (
+    UCConfigParser, get_repositories, get_repository, format_repo)
+
+from elasticgit.tests.base import ModelBaseTest
+
+from git import Repo
 
 
 class TestUCConfigParser(TestCase):
@@ -33,3 +38,30 @@ class TestUCConfigParser(TestCase):
             'b': '2',
             'c': '3',
         })
+
+
+class TestRepositoryUtils(ModelBaseTest):
+
+    def setUp(self):
+        self.workspace = self.mk_workspace()
+
+    def test_get_repositories(self):
+        [repo] = get_repositories(self.WORKING_DIR)
+        self.assertTrue(isinstance(repo, Repo))
+        self.assertEqual(repo.working_dir, self.workspace.working_dir)
+
+    def test_get_repository(self):
+        repo = get_repository(self.workspace.working_dir)
+        self.assertTrue(isinstance(repo, Repo))
+        self.assertEqual(repo.working_dir, self.workspace.working_dir)
+
+    def test_format_repo(self):
+        formatted = format_repo(self.workspace.repo)
+        self.assertEqual(
+            set(formatted.keys()),
+            set(['name', 'commit', 'timestamp', 'author', 'schemas']))
+        last_commit = self.workspace.repo.commit()
+        self.assertEqual(formatted['commit'], last_commit.hexsha)
+        self.assertEqual(
+            formatted['author'], '%s <%s>' % (last_commit.author.name,
+                                              last_commit.author.email))
