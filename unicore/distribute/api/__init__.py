4d4d205eb8941ec54472a2aa8f1b10dbf416fe1a
Simon de Haan <simon@praekeltfoundation.org>
2015-04-09 12:54:40 +0200

    shuffling things around and adding webhooks module

--- unicore/distribute/api/__init__.py
+++ unicore/distribute/api/__init__.py
@@ -3,10 +3,16 @@ from pyramid.config import Configurator
 
 def main(global_config, **settings):
     config = Configurator(settings=settings)
-    config.include('unicore.distribute.api')
+    config.include('unicore.distribute.api', 'foo')
     return config.make_wsgi_app()
 
 
 def includeme(config):
     config.include('cornice')
     config.scan('unicore.distribute.api.repos')
+
+    # Dynamically load any stuff that's optionally included.
+    settings = config.registry.settings
+    api_includes = settings['unicore.distribute.includes'].strip().split('\n')
+    for include in api_includes:
+        config.include(include)
