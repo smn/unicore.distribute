5114630db60192ae61105be17e461101919311f7
Simon de Haan <simon@praekeltfoundation.org>
2015-05-13 20:50:41 +0200

    proxy requests on /esapi/ to elasticsearch upstream

--- unicore/distribute/api/__init__.py
+++ unicore/distribute/api/__init__.py
@@ -1,5 +1,7 @@
 from pyramid.config import Configurator
 
+from unicore.distribute.api import proxy
+
 
 def main(global_config, **settings):
     config = Configurator(settings=settings)
@@ -9,6 +11,6 @@ def main(global_config, **settings):
 
 def includeme(config):
     config.include('cornice')
-    config.add_route('esapi', '/esapi/{parts:.*}')
     config.scan('.repos')
-    config.scan('.search')
+    config.add_route('esapi', '/esapi/{parts:.*}')
+    config.add_view(proxy.Proxy('http://localhost:9200/'), route_name='esapi')
---
+++ unicore/distribute/api/proxy.py
@@ -0,0 +1,53 @@
+from pyramid.response import Response
+from pyramid.httpexceptions import HTTPNotFound
+
+import requests
+
+
+class Proxy(object):
+
+    def __init__(self, upstream_url):
+        self.upstream_url = upstream_url
+
+    def __call__(self, request):
+        view = ProxyView(request, self.upstream_url)
+        handler = getattr(view, 'do_%s' % (request.method,), None)
+        if handler:
+            return handler()
+        return HTTPNotFound()
+
+
+class ProxyView(object):
+
+    def __init__(self, request, upstream_url):
+        self.request = request
+        self.upstream_url = upstream_url
+
+    def url(self):
+        return '%s%s' % (self.upstream_url, self.request.matchdict['parts'])
+
+    def mk_response(self, response):
+        return Response(body=response.text, status=response.status_code,
+                        headerlist=response.headers.items(),
+                        content_type=response.headers['Content-Type'],
+                        charset=response.encoding)
+
+    def mk_request(self):
+        return self.mk_response(
+            requests.request(
+                self.request.method, self.url(), data=self.request.body))
+
+    def do_POST(self):
+        return self.mk_request()
+
+    def do_DELETE(self):
+        return self.mk_request()
+
+    def do_PUT(self):
+        return self.mk_request()
+
+    def do_GET(self):
+        return self.mk_request()
+
+    def do_HEAD(self):
+        return self.mk_request()
