953848a3d84040d8dbcf4961862166e4f3e585bc
Eduardo Koloma Jr <eduardo@kolomajr.com>
2015-06-22 15:34:19 +0200

    refactor: fixed long lines to conform th PEP8

--- unicore/distribute/api/repo_status.py
+++ unicore/distribute/api/repo_status.py
@@ -1,6 +1,7 @@
 import os
 from cornice.resource import resource, view
-from unicore.distribute.utils import get_config, format_repo_status, get_repository, get_repositories
+from unicore.distribute.utils import (get_config, format_repo_status,
+                                      get_repository, get_repositories)
 
 
 @resource(collection_path='status.json', path='/status/{name}.json')
@@ -11,10 +12,29 @@ class RepositoryStatusResource(object):
 
     def collection_get(self):
         storage_path = self.config.get('repo.storage_path')
-        return [format_repo_status(repo) for repo in get_repositories(storage_path)]
+        return [format_repo_status(repo) for repo
+                in get_repositories(storage_path)]
 
     @view(renderer='json')
     def get(self):
         name = self.request.matchdict['name']
         storage_path = self.config.get('repo.storage_path')
-        return format_repo_status(get_repository(os.path.join(storage_path, name)))
+        return format_repo_status(get_repository(
+            os.path.join(storage_path, name)))
+
+
+@resource(path='/status/{name}/{commit_id}.json')
+class RepositoryDiffFilesResource(object):
+    def __init__(self, request):
+        self.request = request
+        self.config = get_config(request)
+
+    @view(renderer='json')
+    def get(self):
+        name = self.request.matchdict['name']
+        commit_id = self.request.matchdict['commit_id']
+        storage_path = self.config.get('repo.storage_path')
+        return {
+            "commit": commit_id,
+            "commit_reversed": commit_id[::-1]
+        }
